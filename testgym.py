11fcbd0612d3be7a19f44a78a5bb3bd7de242cdf
Zach Nordgren <zdnordgren@gmail.com>
2019-10-29 22:45:49 -0500

    got NEAT working for cartpole

---
+++ testgym.py
@@ -0,0 +1,11 @@
+'''
+pip install --no-index -f https://github.com/Kojoley/atari-py/releases atari_py
+'''
+
+import gym
+env = gym.make('SpaceInvaders-v0')
+env.reset()
+for _ in range(1000):
+    env.step(env.action_space.sample())
+    env.render('human')
+env.close()
