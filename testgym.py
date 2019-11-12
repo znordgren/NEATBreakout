ac98d1d1ebe53e0d63c0dbc37fbbd28f0b2313b3
Zach Nordgren <zdnordgren@gmail.com>
2019-11-12 00:39:06 -0600

    current running

--- testgym.py
+++ testgym.py
@@ -3,7 +3,7 @@ pip install --no-index -f https://github.com/Kojoley/atari-py/releases atari_py
 '''
 
 import gym
-env = gym.make('SpaceInvaders-v0')
+env = gym.make('Breakout-ram-v0')
 env.reset()
 for _ in range(1000):
     env.step(env.action_space.sample())
